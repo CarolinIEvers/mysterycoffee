2bf0a9df50e7f86446972dfff08b6f78212d9d58
Et9797 <etet1997@hotmail.com>
2021-03-04 18:58:25 +0100

    first commit

---
+++ mysterycoffee.py
@@ -0,0 +1,53 @@
+import gspread
+import requests
+import pandas as pd
+import random
+import copy
+import sys
+
+
+def fetch_and_pair_participants(max_group_size=2):
+
+    """
+    Fetches the (new) participants from the Google Spreadsheet.
+    Rematching is prevented by storing previous pairings in this spreedsheet. 
+    """
+
+    pass
+
+    # return new_pairings
+
+
+def fetch_conversation_starter():
+    
+    """ 
+    Fetch a conversation starter from Conversationstarter.com
+    """
+
+    pass
+
+
+def email_participants():
+    
+    """
+    To send out emails for example every Monday at 12:00, set up a cronjob (Linux)
+    like so:
+        
+        '0 12 * * 1 python3 /path/to/mysterycoffee.py'
+
+    """
+    pass
+
+
+
+
+
+
+def main():
+
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
